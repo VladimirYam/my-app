8aa1a8ef85f96650d5aaaf7a91a3efe43ad362c6
Vladimir Yam <vovankld39@gmail.com>
2023-09-10 21:20:24 +0200

    Create component CardsCost

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import CostBlock from "./Components/Costs/CostBlock";
+
 function App() {
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description: "Burger",
+      amount: 250,
+    },
+    {
+      date: new Date(2021, 12, 2),
+      description: "Apple",
+      amount: 75,
+    },
+    {
+      date: new Date(2021, 4, 15),
+      description: "Banana",
+      amount: 120,
+    },
+  ];
   return (
     <div>
-      <h1>
-        Hello
-      </h1>
+      <h1>Hello</h1>
+      <CostBlock costs = {costs}/>
     </div>
   );
 }
